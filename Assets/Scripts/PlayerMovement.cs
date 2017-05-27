d684b9fd0c51a898be4e96b7f986b043d82ef33a
Oscar Langford <o.langford4722@student.leedsbeckett.ac.uk>
2017-05-26 23:41:26 -0400

    Network manager set up - testing basic player moevement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    private CharacterController controller;
+    public float speed;
+
+	void Start ()
+	{
+	    controller = GetComponent<CharacterController>();
+	}
+	
+	void Update ()
+	{
+	    if (!isLocalPlayer)
+	    {
+	        return;
+	    }
+
+	    if (Input.GetKey(KeyCode.D))
+	    {
+	        controller.Move(Vector3.right * speed * Time.deltaTime);
+	    }
+        if (Input.GetKey(KeyCode.A))
+        {
+            controller.Move(Vector3.left * speed * Time.deltaTime);
+        }
+    }
+}
