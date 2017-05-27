2e11a6c5ea944634b7da3381cd8db394ffb03f86
Oscar Langford <o.langford4722@student.leedsbeckett.ac.uk>
2017-05-27 00:49:55 -0400

    Networks between two Android devices, with test cube movement working

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,31 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class PlayerMovement : NetworkBehaviour
 {
     private CharacterController controller;
     public float speed;
 
-	void Start ()
-	{
-	    controller = GetComponent<CharacterController>();
-	}
-	
-	void Update ()
-	{
-	    if (!isLocalPlayer)
-	    {
-	        return;
-	    }
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        GameObject.Find("IpAddress").GetComponent<Text>().text = Network.player.ipAddress;
+    }
 
-	    if (Input.GetKey(KeyCode.D))
-	    {
-	        controller.Move(Vector3.right * speed * Time.deltaTime);
-	    }
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        KeyboardControl();
+        TouchControl();
+    }
+
+    void KeyboardControl()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            controller.Move(Vector3.right*speed*Time.deltaTime);
+        }
         if (Input.GetKey(KeyCode.A))
         {
-            controller.Move(Vector3.left * speed * Time.deltaTime);
+            controller.Move(Vector3.left*speed*Time.deltaTime);
+        }
+    }
+
+    void TouchControl()
+    {
+        // cycles though all the fingers touching the screen
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            // if finger is touching the left side of the screen
+            if (Input.GetTouch(i).position.x < Screen.width/2)
+            {
+                controller.Move(Vector3.left * speed * Time.deltaTime);
+            }
+            // if finger is touching the right side of the screen
+            else
+            {
+                controller.Move(Vector3.right * speed * Time.deltaTime);
+            }
         }
     }
 }
